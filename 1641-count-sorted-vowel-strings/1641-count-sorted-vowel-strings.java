5a0f58ffe614ccf54b4543cbaf65279aa0e39f56
Garvit Gupta <garvitgupta063@gmail.com>
2026-09-06 11:28:27 +0530

    Time: 0 ms (100%), Space: 41.9 MB (75.86%) - LeetHub

--- 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.java
+++ 1641-count-sorted-vowel-strings/1641-count-sorted-vowel-strings.java
@@ -2,10 +2,10 @@ class Solution {
     public int countVowelStrings(int n) {
         int a=1,e=1,i=1,o=1,u=1;
         while(--n>0){
-            o+=u;
-            i+=o;
-            e+=i;
-            a+=e;
+           a+=e;
+           i+=a;
+           o+=i;
+           u+=o;
         }
         return a+e+i+o+u;
     }
