c5a7771f8fd0f350fab5eeb50a9355c76c777b03
Garvit Gupta <garvitgupta063@gmail.com>
2026-09-08 08:54:17 +0530

    Time: 2 ms (94.76%), Space: 47.7 MB (48.16%) - LeetHub

---
+++ 0645-set-mismatch/0645-set-mismatch.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int[] findErrorNums(int[] nums) {
+        int n = nums.length;
+        int[] freq = new int[n+1];
+        for(int num : nums){
+            freq[num]++;
+        }
+
+        int missing = -1;
+        int duplicate = -1;
+
+        for(int i=0;i<=n;i++){
+            if(freq[i]==2){
+                duplicate = i;
+            }if(freq[i]==0){
+                missing = i;
+            }
+        }
+        return new int[]{duplicate,missing};
+    }
+}
