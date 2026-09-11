2176748af8e99ceea3fa0d3e79848bc0e2a3a30d
Garvit Gupta <garvitgupta063@gmail.com>
2026-09-11 08:01:05 +0530

    Time: 6 ms (64.3%), Space: 175.5 MB (29.33%) - LeetHub

---
+++ 2090-k-radius-subarray-averages/2090-k-radius-subarray-averages.java
@@ -0,0 +1,28 @@
+class Solution {
+    public int[] getAverages(int[] nums, int k) {
+        int n = nums.length;
+        int[] arr = new int[n];
+        Arrays.fill(arr,-1);
+         
+
+        int w = 2*k+1;
+        if(n<w){
+            return arr;
+        }
+
+        long sum = 0;
+        for(int i=0;i<w;i++){
+            sum += nums[i];
+        }
+        arr[k] = (int)(sum/w);
+
+        for(int i=w;i<n;i++){
+            sum += nums[i];
+            sum -= nums[i-w];
+
+            int center = i - k;
+            arr[center] = (int)(sum / w);
+        }
+        return arr;
+    }
+}
