811109c07ca0e62dd9f8abf9285a4b28c4014eeb
Garvit Gupta <garvitgupta063@gmail.com>
2026-09-23 07:54:13 +0530

    Time: 1 ms (26.65%), Space: 42.8 MB (33.9%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -0,0 +1,39 @@
+class Solution {
+    public boolean checkValidString(String s) {
+        Stack<Integer> st = new Stack<>();
+        Stack<Integer> star = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+
+            if (ch == '(') {
+                st.push(i);
+            } 
+            else if (ch == '*') {
+                star.push(i);
+            } 
+            else { 
+                if (!st.isEmpty()) {
+                    st.pop();
+                } 
+                else if (!star.isEmpty()) {
+                    star.pop();
+                } 
+                else {
+                    return false;
+                }
+            }
+        }
+
+        while (!st.isEmpty() && !star.isEmpty()) {
+            if (st.peek() > star.peek()) {
+                return false;
+            }
+
+            st.pop();
+            star.pop();
+        }
+
+        return st.isEmpty();
+    }
+}
